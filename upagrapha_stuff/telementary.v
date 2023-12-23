d324bb784f5c5c0d1b2e877b799dc1f2ccda66b5
faisalill <mfaisal.ilyas.d@gmail.com>
2023-12-23 13:01:23 +0530

    Fix Typo

--- upagrapha_stuff/telementary.v
+++ upagrapha_stuff/telementary.v
@@ -2,19 +2,19 @@ include "helper.v"
 
 reg [31:0] ASM = 32'h1ACFFC1D;
 reg [31:0] FH = 32'hFFEEDD;
-reg [31:0] FH = 32'hFFEEDD00;
-reg [31:0] FH = 32'hFFEEDD01;
-reg [31:0] FH = 32'hFFEEDD02;
-reg [31:0] FH = 32'hFFEEDD03;
-reg [31:0] FH = 32'hFFEEDD04;
-reg [31:0] FH = 32'hFFEEDD05;
-reg [31:0] FH = 32'hFFEEDD06;
-reg [31:0] FH = 32'hFFEEDD07;
+reg [31:0] FH1 = 32'hFFEEDD00;
+reg [31:0] FH2 = 32'hFFEEDD01;
+reg [31:0] FH3 h= 32'hFFEEDD02;
+reg [31:0] FH4 = 32'hFFEEDD03;
+reg [31:0] FH5 = 32'hFFEEDD04;
+reg [31:0] FH6 = 32'hFFEEDD05;
+reg [31:0] FH7 = 32'hFFEEDD06;
+reg [31:0] FH8 = 32'hFFEEDD07;
 
 reg [31:0] x [0:1151] [0:5];
 fill_zeroes(1152, 6, x);
 reg [31:0] txCRC [0:1167] [0:5];
-fill_zeroes(1168 6 ,txCRC);
+fill_zeroes(1168, 6 ,txCRC);
 reg [31:0] crcCodeword [0:5] [0:1167];
 fill_zeroes(6, 1168, crcCodeword);
 reg [31:0] rxMajorFrame [0:5] [0:1151];
@@ -73,7 +73,7 @@ reg [31:0] lastCmd [0:5] [0:255];
 reg [31:0] ii; 
 reg random_bit ;
 for (ii = 0; ii < iiloop; ii = ii + 1) begin
-  fill_random_one_or_zero(6, 128, xGyrol); 
+  fill_random_one_or_zero(6, 128, xGyro); 
   fill_random_one_or_zero(6, 128, yGyro);
   fill_random_one_or_zero(6, 128, zGyro);
   fill_random_one_or_zero(6, 64, q1);
