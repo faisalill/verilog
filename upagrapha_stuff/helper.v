16e6d23415ad4de43df84b6cd436349725403308
faisalill <mfaisal.ilyas.d@gmail.com>
2023-12-23 12:56:55 +0530

    Add Helper

---
+++ upagrapha_stuff/helper.v
@@ -0,0 +1,25 @@
+function void fill_zeroes (input integer rows, input integer cols, output reg [31:0] array [0:rows-1][0:cols-1]);
+
+    for (integer i = 0; i < rows; i = i + 1) begin
+        for (integer j = 0; j < cols; j = j + 1) begin
+            array[i][j] <= 32'b0;  // Assign zero to each element
+        end
+    end
+
+endfunction
+
+function void fill_random_one_or_zero (input integer rows, input integer cols, outpute reg [31:0] array [0:rows-1][0:cols-1]);
+  integer i,j;
+  reg random_bit;  
+
+  for (i = 0; i < rows; i = i + 1) begin
+      for (j = 0; j < cols; j = j + 1) begin
+        // Generate a random bit (0 or 1)
+        random_bit = $random % 2;
+
+        // Assign the random bit to the array element
+        array[i][j] = random_bit;
+      end
+    end
+
+endfunction
--- upagrapha_stuff/telementary.v
+++ upagrapha_stuff/telementary.v
@@ -1,3 +1,85 @@
-reg [31:0] ASM = 32'h1ACFFC1D;
+include "helper.v"
 
-reg [31:0]
+reg [31:0] ASM = 32'h1ACFFC1D;
+reg [31:0] FH = 32'hFFEEDD;
+reg [31:0] FH = 32'hFFEEDD00;
+reg [31:0] FH = 32'hFFEEDD01;
+reg [31:0] FH = 32'hFFEEDD02;
+reg [31:0] FH = 32'hFFEEDD03;
+reg [31:0] FH = 32'hFFEEDD04;
+reg [31:0] FH = 32'hFFEEDD05;
+reg [31:0] FH = 32'hFFEEDD06;
+reg [31:0] FH = 32'hFFEEDD07;
+
+reg [31:0] x [0:1151] [0:5];
+fill_zeroes(1152, 6, x);
+reg [31:0] txCRC [0:1167] [0:5];
+fill_zeroes(1168 6 ,txCRC);
+reg [31:0] crcCodeword [0:5] [0:1167];
+fill_zeroes(6, 1168, crcCodeword);
+reg [31:0] rxMajorFrame [0:5] [0:1151];
+fill_zeroes(6, 1152, rxMajorFrame);
+reg [31:0] y [0:1151] [0:5];
+fill_zeroes(1152, 6, y);
+reg [31:0] rxCRC [0:1167] [0:5];
+fill_zeroes(1168, 6, rxCRC);
+reg [31:0] rxCrcCodeword [0:5] [0:1167];
+fill_zeroes(6, 1168, rxCrcCodeword);
+reg [31:0] crcCompare [0:0] [0:5];
+fill_zeroes(1, 6, crcCompare);
+reg [31:0] SNR = 32'd4; 
+reg [31:0] match [0:0] [0:5];
+fill_zeroes(1, 6, match);
+reg [31:0] mismatch [0:0] [0:5];
+fill_zeroes(1, 6, mismatch);
+
+reg [31:0] totalmatch = 32'd0;
+reg [31:0] totalmismatch = 32'd0;
+reg [31:0] cycleBER = 32'd0;
+reg [31:0] xGyroRx [0:5] [0:127];
+fill_zeroes(6, 128, xGyroRx);
+reg [31:0] yGyroRx [0:5] [0:127];
+fill_zeroes(6, 128, yGyroRx);
+reg [31:0] zGyroRx [0:5] [0:127];
+fill_zeroes(6, 128, zGyroRx);
+reg [31:0] q1Rx [0:5] [0:63];
+fill_zeroes(6, 64, q1Rx);
+reg [31:0] q2Rx [0:5] [0:63];
+fill_zeroes(6, 64, q2Rx);
+reg [31:0] q3Rx [0:5] [0:63];
+fill_zeroes(6, 64, q3Rx);
+reg [31:0] q4Rx [0:5] [0:63];
+fill_zeroes(6, 64, q4Rx);
+reg [31:0] lastCmdRx [0:5] [0:255];
+fill_zeroes(6, 256, lastCmdRx);
+reg [31:0] tmNoOfErrors = 32'd0;
+reg [31:0] iiloop = 32'd100;
+reg [31:0] tmSuccessfullyReceivedBits [0:0] [0:iiloop];
+fill_zeroes(1, iiloop+1, tmSuccessfullyReceivedBits);
+reg [31:0] tmUnsuccessfullyReceivedBits [0:0] [0:iiloop];
+fill_zeroes(1, iiloop+1, tmUnsuccessfullyReceivedBits);
+reg [31:0] tmTransmittedbits [0:0] [0:iiloop];
+fill_zeroes(1, iiloop+1, tmTransmittedbits);
+
+reg [31:0] xGyro [0:5] [0:127];
+reg [31:0] yGyro [0:5] [0:127];
+reg [31:0] zGyro [0:5] [0:127];
+reg [31:0] q1 [0:5] [0:63];
+reg [31:0] q2 [0:5] [0:63];
+reg [31:0] q3 [0:5] [0:63];
+reg [31:0] q4 [0:5] [0:63];
+reg [31:0] lastCmd [0:5] [0:255];
+
+reg [31:0] ii; 
+reg random_bit ;
+for (ii = 0; ii < iiloop; ii = ii + 1) begin
+  fill_random_one_or_zero(6, 128, xGyrol); 
+  fill_random_one_or_zero(6, 128, yGyro);
+  fill_random_one_or_zero(6, 128, zGyro);
+  fill_random_one_or_zero(6, 64, q1);
+  fill_random_one_or_zero(6, 64, q2);
+  fill_random_one_or_zero(6, 64, q3);
+  fill_random_one_or_zero(6, 64, q4);
+  fill_random_one_or_zero(6, 256, lastCmd);
+end
+  
